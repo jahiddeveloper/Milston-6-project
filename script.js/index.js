3997f967bf59c24dfff92dacd190064fcfb95c23
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-09-06 16:53:57 +0600

    cart-compleat

--- script.js/index.js
+++ script.js/index.js
@@ -1,9 +1,57 @@
-
 const loadLesson = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((response) => response.json())
     .then((data) => displayLesson(data.data));
 };
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayLevelWord(data.data));
+};
+
+// {
+//     "id": 84,
+//     "level": 1,
+//     "word": "Fish",
+//     "meaning": "মাছ",
+//     "pronunciation": "ফিশ"
+// }
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+  for (const word of words) {
+    console.log(word);
+    const cart = document.createElement("div");
+    cart.innerHTML = `
+    <div class="bg-white py-10 px-5 rounded-xl shadow-sm text-center">
+        <h2 class="font-bold text-xl mb-6">${word.word}</h2>
+        <p class="text-[1.25rem]">Meaning /Pronounciation</p>
+        <div class="mt-6 text-[#18181B] text-3xl font-bold">
+          ${word.meaning} / ${word.pronunciation}
+        </div>
+        <div class="flex justify-between items-center mt-8">
+          <button
+            class="font-bangla bg-[#1a91ff1a] hover:bg-[#1a91ff33] cursor-pointer btn rounded-lg text-xl"
+          >
+            <i class="fa-solid fa-circle-info"></i>
+          </button>
+          <button
+            class="font-bangla bg-[#1a91ff1a] hover:bg-[#1a91ff33] cursor-pointer btn rounded-lg text-xl"
+          >
+            <i class="fa-solid fa-volume-high"></i>
+          </button>
+        </div>
+      </div>
+    `;
+
+    wordContainer.append(cart);
+  }
+};
+
 const displayLesson = (lessons) => {
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
@@ -11,7 +59,7 @@ const displayLesson = (lessons) => {
   for (const lesson of lessons) {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-                <button class="btn btn-outline btn-primary"
+                <button onClick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"
                 ><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button
                 >`;
 
