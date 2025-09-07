3eff10a1360b684ff0c44a68b382ef0eff00c2d8
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-09-07 08:36:23 +0600

    better-UX

--- script.js/index.js
+++ script.js/index.js
@@ -21,17 +21,38 @@ const loadLevelWord = (id) => {
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
-    wordContainer.innerHTML = "";
+  wordContainer.innerHTML = "";
+
+  if (words.length === 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center col-span-full py-12">
+        <div class="flex justify-center">
+          <img class="mb-4" src="./assets/alert-error.png" alt="" />
+        </div>
+        <p class="font-bangla text-[#79716b] mb-3">
+          এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+        </p>
+        <h3 class="font-bangla text-3xl font-semibold">
+          নেক্সট <span>Lesson</span> এ যান
+        </h3>
+      </div>
+    `;
+    return;
+  }
 
   for (const word of words) {
     console.log(word);
     const cart = document.createElement("div");
     cart.innerHTML = `
     <div class="bg-white py-10 px-5 rounded-xl shadow-sm text-center">
-        <h2 class="font-bold text-xl mb-6">${word.word}</h2>
+        <h2 class="font-bold text-xl mb-6">${
+          word.word ? word.word : "শব্দ পাওয়া যায়নি"
+        }</h2>
         <p class="text-[1.25rem]">Meaning /Pronounciation</p>
-        <div class="mt-6 text-[#18181B] text-3xl font-bold">
-          ${word.meaning} / ${word.pronunciation}
+        <div class="mt-6 text-[#18181B] text-2xl font-bold">
+          ${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${
+      word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"
+    }
         </div>
         <div class="flex justify-between items-center mt-8">
           <button
